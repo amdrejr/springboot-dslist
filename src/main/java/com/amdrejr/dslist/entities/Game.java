3652ec3c413b4e3e3cfe1f00cbe447b0d2d9edea
André Jr <andrejr.mello@hotmail.com>
2023-11-14 14:19:21 -0300

    add import.sql

--- src/main/java/com/amdrejr/dslist/entities/Game.java
+++ src/main/java/com/amdrejr/dslist/entities/Game.java
@@ -17,20 +17,24 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String LongDescription;
 
     public Game () {}
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         LongDescription = longDescription;
@@ -68,12 +72,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public String getImgUrl() {
@@ -100,6 +104,14 @@ public class Game {
         LongDescription = longDescription;
     }
 
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -124,7 +136,6 @@ public class Game {
             return false;
         return true;
     }
-
 
     
 }
